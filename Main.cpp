6e3d1b60a9d90cbbb97b9a5c12a2569094895575
ViruzUniT <73714544+ViruzUniT@users.noreply.github.com>
2023-07-20 00:52:21 +0200

    added more offsets and tried to make console

--- Main.cpp
+++ Main.cpp
@@ -3,26 +3,46 @@
 #define STATUS_INJECTED 0
 #define STATUS_UNINJECTED 1
 
-class Player {
+class Vector3 {
 public:
-    char pad_0000[118]; //0x0000
-    int8_t state; //0x0076
-    int8_t visable; //0x0077
-    char pad_0078[116]; //0x0078
-    int32_t health; //0x00EC
-    char pad_00F0[80]; //0x00F0
+    float x, y, z;
+};
+class Vector2 {
+public:
+    float x, y;
 };
 
+class Player
+{
+public:
+    char pad_0000[4]; //0x0000
+    Vector3 coords; //0x0004
+    Vector3 dir_velocity; //0x0010
+    Vector3 unkown_vector; //0x001C
+    Vector2 s_coords; //0x0028
+    char pad_0030[70]; //0x0030
+    uint8_t state; //0x0076
+    uint8_t visable; //0x0077
+    char pad_0078[116]; //0x0078
+    uint32_t health; //0x00EC
+    char pad_00F0[80]; //0x00F0
+    uint32_t ammo; //0x0140
+    char pad_0144[295]; //0x0144
+}; //Size: 0x026B
+
 namespace GameFunctions {
+    constexpr uintptr_t big_printf = 0x45D890;
+    using big_printf_t = void(*)(const char* format, ...);
+
     constexpr uintptr_t printf = 0x4DAD50;
     using printf_t = void(*)(const char* format, ...);
 };
 
 void SHOW_INJECTION_STATUS(const int status) {
-    const auto printf = reinterpret_cast<GameFunctions::printf_t>(GameFunctions::printf);
+    const GameFunctions::big_printf_t printf = reinterpret_cast<GameFunctions::big_printf_t>(GameFunctions::big_printf);
     if (status == STATUS_INJECTED) {
         printf("Injected");
-        MessageBoxW(
+        MessageBox(
             NULL,
             L"Injected",
             L"MIAU",
@@ -30,7 +50,7 @@ void SHOW_INJECTION_STATUS(const int status) {
         );
     } else if (status == STATUS_UNINJECTED) {
         printf("Uninjected");
-        MessageBoxW(
+        MessageBox(
             NULL,
             L"Uninjected",
             L"MIAU",
@@ -42,13 +62,25 @@ void SHOW_INJECTION_STATUS(const int status) {
 int GameLoop() {
     const uintptr_t client = reinterpret_cast<uintptr_t>(GetModuleHandle(L"ac_client.exe"));
     Player* player = *(reinterpret_cast<Player**>(client + 0x17E0A8));
+    DWORD error;
+
+    if (AllocConsole() == 0) {
+        error = GetLastError();
+        return EXIT_FAILURE;
+    }
 
     while (true) {
         std::this_thread::sleep_for(std::chrono::milliseconds(1));
         if (GetAsyncKeyState(VK_INSERT))
             return EXIT_SUCCESS;
 
-        player->health = 10;
+        if (GetAsyncKeyState(VK_F1) && player->state == 0 && player->health >= 0)
+            player->health = 100;
+        
+        std::cout << player->coords.x << " " << player->coords.y << " " << player->coords.z << " " << std::endl <<
+            player->health << std::endl <<
+            player->ammo << std::endl <<
+            player->state << std::endl;
     }
 }
 
@@ -58,15 +90,16 @@ void injected_thread(const HMODULE instance) {
     int Loop = GameLoop();
 
     // uninject
-    SHOW_INJECTION_STATUS(STATUS_UNINJECTED);
+    FreeConsole();
     FreeLibraryAndExitThread(instance, EXIT_SUCCESS);
+    SHOW_INJECTION_STATUS(STATUS_UNINJECTED);
 }
 
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved) {
     if (fdwReason == DLL_PROCESS_ATTACH) {
         DisableThreadLibraryCalls(hinstDLL);
 
-        const auto thread= CreateThread(nullptr, 0, reinterpret_cast<LPTHREAD_START_ROUTINE>(injected_thread), hinstDLL, 0, NULL);
+        const HANDLE thread= CreateThread(nullptr, 0, reinterpret_cast<LPTHREAD_START_ROUTINE>(injected_thread), hinstDLL, 0, NULL);
 
         if (thread) {
             CloseHandle(thread);
